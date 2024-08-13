3de92b01cb2488f24d475b7a7605bdf02ce8be95
nickkossolapov <nickkossolapov@gmail.com>
2024-08-13 19:41:33 +0100

    Add basic headers

---
+++ asteroids/src/entities/spaceship.h
@@ -0,0 +1,11 @@
+#ifndef ASTEROIDS_SPACESHIP_H
+#define ASTEROIDS_SPACESHIP_H
+
+#include <SDL.h>
+
+class Spaceship {
+public:
+    void handleEvent(const SDL_Event &e);
+};
+
+#endif //ASTEROIDS_SPACESHIP_H
